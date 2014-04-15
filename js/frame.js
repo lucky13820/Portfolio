f3153e5bb74c93c0b85c19ce950be0da1be92e9f
lucky13820 <lucky13820@gmail.com>
2014-04-14 17:42:54 -0700

    site

---
+++ js/frame.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+var cpo = []; cpo["_object"] ="cp_widget_becbaa93-f384-4116-b2b3-9bf0e5189320"; cpo["_fid"] = "A4JACg7hn7Uq";
+var _cpmp = _cpmp || []; _cpmp.push(cpo);
+(function() { var cp = document.createElement("script"); cp.type = "text/javascript";
+cp.async = true; cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
+var c = document.getElementsByTagName("script")[0];
+c.parentNode.insertBefore(cp, c); })(); 
